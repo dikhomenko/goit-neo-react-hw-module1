b65285b96c423b62d5fdcc94820de653c0dee527
dkhomenko <dinamastercs@gmail.com>
2024-11-15 15:24:46 +0100

    add task 2 - FriendList

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Profile from './components/Profile/Profile';
+import FriendList from './components/FriendList/FriendList';
 import userData from './userData.json';
+import friends from './friends.json';
 
 const App = () => {
   return (
@@ -12,6 +14,7 @@ const App = () => {
         image={userData.avatar}
         stats={userData.stats}
       />
+      <FriendList friends={friends} /> {}
     </div>
   );
 };
